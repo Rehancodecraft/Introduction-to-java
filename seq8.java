309968b173af03322c3e3459fce7d8a846c45380
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-19 17:28:11 +0500

    It defines how to print table of 5

---
+++ seq8.java
@@ -0,0 +1,10 @@
+public class seq8 {
+    public static void main(String[] args) {
+        int a = 5;
+        for (int i = 1; i <=10; i++)
+        {    
+        System.out.println(a+"*"+i + "="+ a * i);
+        }
+    }
+    
+}
