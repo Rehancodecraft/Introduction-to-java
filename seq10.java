274bb88d8c99dfb2a5319bfb9e2b3678cb322c70
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-19 17:30:25 +0500

    It defines what will be output if we use two statements just after the for loop

---
+++ seq10.java
@@ -0,0 +1,8 @@
+public class seq10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++)
+        System.out.println(i);
+        System.out.println("hello");
+    }
+    
+}
