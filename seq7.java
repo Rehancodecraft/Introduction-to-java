5e6d3d35bed8864b6e844b626f33a2cf9751f344
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-19 17:27:27 +0500

    It defines how to print only even natural numbers

---
+++ seq7.java
@@ -0,0 +1,9 @@
+ public class seq7{
+    public static void main(String[] args) {
+        for (int i = 20; i <=100; i++)
+            if (i % 2 == 0)
+            {
+            System.out.println(i);
+            }
+    }
+ }
