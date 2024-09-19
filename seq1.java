b0dc492e08447e0bbac4b2fe38814a04fb933f62
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-19 17:21:43 +0500

    It defines condition

---
+++ seq1.java
@@ -0,0 +1,11 @@
+public class seq1{
+    public static void main(String[]args){
+        int a = 10;
+        int b = 5;
+        if (a <b)
+        System.out.println("Good");
+        else
+        System.out.println("Bad");
+        System.out.println("hello");
+    }
+}
