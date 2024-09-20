ade721636afa408b1be5f06715e6178c9ed9e4d9
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-20 10:29:02 +0500

    This is Activity1 of Lab2

---
+++ seq11.java
@@ -0,0 +1,9 @@
+public class seq11 {
+    public static void main(String[] args) {
+        int i;
+        for ( i = 1; i <=5; i++)
+            System.out.println("hello");
+            System.out.println(i);
+    }
+    
+}
