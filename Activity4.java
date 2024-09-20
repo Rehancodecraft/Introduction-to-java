71fd26cb8b7d679e1a788334a53afab2288b2335
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-20 10:34:20 +0500

    This is Activity4 of Lab 2

---
+++ Activity4.java
@@ -0,0 +1,10 @@
+public class Activity4 {
+    public static void main(String[] args) {
+        char letter;
+        letter = 65;
+        System.out.println(letter);
+        letter = 66;
+        System.out.println(letter);
+    }
+    
+}
