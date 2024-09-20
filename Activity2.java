4b9f74b5fa8d5905195c893723c2b7d7f8571318
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-20 10:32:08 +0500

    This is Activity2 of Lab 2

---
+++ Activity2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Activity2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int feet;
+        int inches;
+        System.out.println("Enter two integers separated by spaces.");
+        feet = console.nextInt();
+        inches = console.nextInt();
+        System.out.println("feet = " + feet);
+        System.out.println("inches = " + inches);
+    }
+    
+}
