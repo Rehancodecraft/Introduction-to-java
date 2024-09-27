ea01305e4430a8398948d34d8d16ff6fb9fa3369
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-27 23:02:57 +0500

    This is how we made patterns in java

---
+++ pattern1.java
@@ -0,0 +1,16 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++)
+        {   
+            for(int k = 5; k > i; k--)
+            {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=2*i-1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
