937dabe0acc5fbd63490093d16fdf3f722fb3634
Rehancodecraft <rehan.codecraft@gmail.com>
2024-09-28 20:17:57 +0500

    This is Lab2 of Java

---
+++ Lab2/Activity1.java
@@ -0,0 +1,17 @@
+public class Activity1 {
+    public static void main(String[] args) {
+        int num1,num2;
+        double sale;
+        char first;
+        num1 = 4;
+        System.out.println("Num1: " + num1);
+        num2 = 4 * 5 - 11;
+        System.out.println("Num: " + num2);
+        sale = 0.02 * 1000;
+        System.out.println("Sale: " + sale);
+        first = 'D';
+        System.out.println("First: " + first);
+
+    }
+    
+}
---
+++ Lab2/LabTask1.java
@@ -0,0 +1,14 @@
+public class LabTask1 {
+    public static void main(String[] args) {
+        int num1, num2, num3, average;
+        num1 = 125;
+        num2 = 28;
+        num3 = -25;
+        average = (num1 + num2 + num3)/ 3;
+        System.out.println("Num1: " + num1);
+        System.out.println("Num2: " + num2);
+        System.out.println("Num3: " + num3);
+        System.out.println("Average: " + average);
+    }
+    
+}
