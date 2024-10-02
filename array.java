ac84f44b8b2d6feab888ed49dc236e8ceae33e9b
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-02 20:37:40 +0500

    These are arrays in java

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int [] a = new int [5];
+        int [] b = new int []{20,30,40,60};
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+        a[4] = 50;
+        
+    }
+}
