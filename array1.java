b8ed6c5eedd4b42e52a9c99d510d5e148bbc6a3b
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-02 20:38:00 +0500

    These are arrays in java

---
+++ array1.java
@@ -0,0 +1,17 @@
+public class array1 {
+    public static void main(String[] args) {
+        int []marks = new int [5];
+        marks [0] = 75;
+        marks [1] = 85; 
+        marks [2] = 79;
+        marks [3] = 83;
+        marks [4] = 73;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+
+    }
+    
+}
