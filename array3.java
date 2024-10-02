52852bd39d276ae7510f27b65fd1c0ad4d763a9f
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-02 20:45:12 +0500

    These are arrays in java

---
+++ array3.java
@@ -0,0 +1,11 @@
+public class array3 {
+    public static void main(String[] args) {
+        String [] subjects = {"English", "chemistry", "physics", "Maths", "urdu"};
+        System.out.println(subjects[0]);
+        System.out.println(subjects[1]);
+        System.out.println(subjects[2]);
+        System.out.println(subjects[3]);
+        System.out.println(subjects[4]);
+    }
+    
+}
