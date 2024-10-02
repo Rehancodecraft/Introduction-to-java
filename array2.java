c4f0e4781e853723187c4d5d6826199e537178b0
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-02 20:38:21 +0500

    These are arrays in java

---
+++ array2.java
@@ -0,0 +1,12 @@
+public class array2 {
+    public static void main(String[] args) {
+        int []marks = {75,85,546,56,45};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+
+    }
+    
+}
