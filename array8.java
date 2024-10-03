24864adc0d5527d732693deb77c00e450c53fcf6
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-03 19:02:48 +0500

    This is how we can find sum of all elements of an array

---
+++ array8.java
@@ -0,0 +1,11 @@
+public class array8 {
+    public static void main(String[] args) {
+        int [] arr = {20,10,30,50,15};
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum: " + sum);
+    }
+    
+}
