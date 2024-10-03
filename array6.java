db4ee738a16fafb4dbf08bc8c8e756ba38a7363f
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-03 19:01:33 +0500

    This is how to find Minimum number in an array

--- array6.java
+++ array6.java
@@ -1,3 +1,13 @@
 public class array6 {
+    public static void main(String[] args) {
+        int [] arr = {20,10,30,50,15};
+        int  min = arr[0];
+        for(int i = 1; i < 5; i++){
+            if (arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum Value = " + min);
+    }
     
 }
