761e38b5ec5fc8ed06612c5afa49b49f94e8abc8
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-05 15:18:32 +0500

    Modified

--- array5.java
+++ array5.java
@@ -4,12 +4,13 @@ public class array5 {
         String []name = new String[5];
         int []age = new int [5];
         Scanner input = new Scanner(System.in);
-        for ( int i = 0; i < 5; i++){
-            System.out.print("Enter name of " +(i+1)+ "Student: ");
+        for ( int i = 0; i < 5; i++){ 
+            System.out.print("Enter name of " +(i+1)+ " Student: ");
             name[i] = input.nextLine();
-            System.out.print("Enter age of " + (i+1)+ "Student: ");
-            age[i]= input.nextInt();
+        } 
+        for(int j = 0; j< 5; j++){
+            System.out.print("Enter age of " + (j+1)+ " Student: ");
+            age[j]= input.nextInt();
         }
-        
     }
 }
