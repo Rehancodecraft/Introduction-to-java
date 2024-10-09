5317bd41feaf5979210475da16d7b7d3cba62c7b
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-10 01:17:06 +0500

    using simple conditions

---
+++ simpleCondition.java
@@ -0,0 +1,9 @@
+public class simpleCondition {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        int c  = a<b?a:b;
+        System.out.println(a);
+    }
+    
+}
