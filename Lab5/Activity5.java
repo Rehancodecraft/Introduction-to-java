45c11552f5c3f66def44d6bb5d7a4d1fc1cb7157
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-11 13:44:16 +0500

    This is Activity5 of Lab5 of java

---
+++ Lab5/Activity5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Activity5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number, sum = 0, count;
+        for (count = 0; count < 5; count++){
+            number = input.nextInt();
+            sum += number;
+        }
+        System.out.println("sum is "+ sum);
+    }
+    
+}
