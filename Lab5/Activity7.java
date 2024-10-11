50de42e09708142acb4db04922b38eebad6309d3
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-11 13:46:25 +0500

    This is Activity7 of Lab5 of java

---
+++ Lab5/Activity7.java
@@ -0,0 +1,16 @@
+public class Activity7 {
+    public static void main(String[] args) {
+        System.out.println("        Multipplication Table");
+        System.out.print("    ");
+        for(int j = 1; j <=9; j++)
+            System.out.print("  " + j);
+            System.out.println("\n______________________________________");
+        for (int i = 1; i <= 9; i++){
+            System.out.print(i + "|");
+            for(int j = 1; j <=9; j++){
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }   
+}
