f631df0507a011740890f9cee22d4ecf24bfb5a0
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-16 23:57:52 +0500

    This is how we creat 2D arrays in java

---
+++ array2d1.java
@@ -0,0 +1,9 @@
+public class array2d1 {
+    public static void main(String[] args) {
+        int [][] arr = new int[3][3];
+        for(int i = 0 ; i < 3; i++){
+                 
+        }
+    }
+    
+}
