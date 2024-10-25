7d4b7fe6cb580ffa5eab1d251c1126d50663e0d2
Rehancodecraft <rehan.codecraft@gmail.com>
2024-10-25 23:50:56 +0500

    I have modified

--- diamond.java
+++ diamond.java
@@ -1,26 +1,30 @@
 public class diamond {
     public static void main(String[] args) {
-        for( int i = 1; i<=5; i++)
-        {
-            for( int k = 1;k>=i;k--){
+        // Upper half of the diamond
+        for( int i = 1; i <= 5; i++) {
+            // Print spaces
+            for( int k = 5; k > i; k--) {
                 System.out.print(" ");
             }
-            for(int j = 1; j<=i; j++){
+            // Print stars
+            for(int j = 1; j <= i*2-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int a = 4; a>=1; a--)
-        {
-            for(int b = 5; b>a; b--);
-                {
-                    System.out.print(" ");
-                }
-            for(int c = 1; c<=a;c++){
+
+        // Lower half of the diamond
+        for(int a = 4; a >= 1; a--) {
+            // Print spaces
+            for(int b = 5; b > a; b--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for(int c = 1; c <= a*2-1; c++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    
 }
+
