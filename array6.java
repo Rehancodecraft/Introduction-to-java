244d15796e40fb240aa7728dd492741bb4eba081
Rehancodecraft <rehan.codecraft@gmail.com>
2024-11-12 23:49:54 +0500

    Modified

--- array6.java
+++ array6.java
@@ -1,5 +1,7 @@
 public class array6 {
     public static void main(String[] args) {
+    //This is how we use arrays in java
+    
         int [] arr = {20,10,30,50,15};
         int  min = arr[0];
         for(int i = 1; i < 5; i++){
