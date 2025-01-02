7f0bd318ac0895677b2d782345d24571456da004
Rehancodecraft <rehan.codecraft@gmail.com>
2025-01-02 13:04:49 +0500

    Modified

---
+++ Filecreating.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Filecreating{
+	public static void main(String[] args) {
+		try{
+			File file = new File("MyFirstFile.txt");
+		if(file.createNewFile()){
+			System.out.println("File created");
+		}
+		else
+		{
+			System.out.println("File already exist");
+		}
+		}
+		catch(IOException e){
+			System.out.println("Error occured");
+			e.printStackTrace();
+		}
+	}
+}
