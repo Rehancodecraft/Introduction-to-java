84742ba7850b84e521993b145931e6010f5e3083
rehancodecraft <rehan.codecraft@gmail.com>
2025-01-13 02:56:32 +0500

    Modified

--- seq11.java
+++ seq11.java
@@ -1,6 +1,7 @@
 public class seq11 {
     public static void main(String[] args) {
         int i;
+	//this is a for loop
         for ( i = 1; i <=5; i++)
             System.out.println("hello");
             System.out.println(i);
